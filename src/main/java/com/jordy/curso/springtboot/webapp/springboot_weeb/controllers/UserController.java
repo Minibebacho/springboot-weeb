71da4b10f633e02f5bd6abf5b3ad0b5db55066a9
Jordy Espinoza Camacho <jordyfelipe880@gmail.com>
2025-12-26 22:35:50 -0500

    clase 1

--- src/main/java/com/jordy/curso/springtboot/webapp/springboot_weeb/controllers/UserController.java
+++ src/main/java/com/jordy/curso/springtboot/webapp/springboot_weeb/controllers/UserController.java
@@ -1,16 +1,29 @@
 package com.jordy.curso.springtboot.webapp.springboot_weeb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model ){
+        model.addAttribute("title", "Hola mundo");
+        model.addAttribute("name", "Jordy");
+        model.addAttribute("lastname", "Felipe");
+
         return "details";
     }
 
+    @GetMapping ("/sandy")
+    public String sandy(Model model){
+        model.addAttribute("title", "perro");
+        model.addAttribute("nombre", "Sandy");
+        model.addAttribute("raza", "chihuahua");
+        return "sandy";
+    }
+    
     }
 
 
